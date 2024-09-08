8ac10439b6f54d4e625d5aaf717c155fe7173712
vibhor <vibgitcode27@gmail.com>
2024-09-09 03:55:45 +0530

    added vanish input

--- src/app/(pages)/chat/page.tsx
+++ src/app/(pages)/chat/page.tsx
@@ -1,7 +1,19 @@
 "use client";
 import { Button , Card, Flex, Typography } from "antd";
+import { PlaceholdersAndVanishInput } from "@/app/components/vanishInput";
 
 export default function ChatPage() {
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log("Input changed:", e.target.value);
+    };
+
+    const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("Form submitted!");
+    };
+
+    const placeholders = ["Ask", "Know", "Placeholder 3"];
+    
     return (
         <div className="h-[50rem] w-full dark:bg-black bg-black  dark:bg-grid-black/[0.2] bg-grid-white/[0.2] relative flex items-center justify-center">
             <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,white)]"></div>
@@ -10,7 +22,14 @@ export default function ChatPage() {
                 <Flex justify="center" align="center" vertical>
                     <h1 style={{ color : "white" , fontSize : "65px" , fontWeight : "600" , marginBottom : "-30px"}}>Your <span className="glow">Personal</span></h1>
                     <h1 style={{ color : "white" , fontSize : "65px" , fontWeight : "600"}}>AI Advisor</h1>
-                    
+                    <Card style={{ width : "850px" , backgroundColor : "transparent" , border : "none" , height : "600px"}}>
+
+                    </Card>
+                    <PlaceholdersAndVanishInput
+                        placeholders={placeholders}
+                        onChange={handleInputChange}
+                        onSubmit={handleFormSubmit}
+                    />
                 </Flex>
             </Card>
         </div>
